e3ccc3604e7e31549da4d60354544bd393a1b6e6
THE LAPTOP SHOP <abdallahmohamedabueleneien@gmail.com>
2024-09-01 19:07:38 +0300

    app init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+ import 'package:flutter/cupertino.dart';
+
+void main()
+ {
+   runApp(MyAPP());
+ }
+ 
+ 
+ 
+ class MyAPP extends StatelessWidget {
+   const MyAPP({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return const Placeholder();
+   }
+ }
+ 
