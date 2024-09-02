28c607095743dd17ac0ee74ff586673f7f939b5b
THE LAPTOP SHOP <abdallahmohamedabueleneien@gmail.com>
2024-09-02 13:02:13 +0300

    app init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
  import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:movies_app/home/home.dart';
 
-void main()
+void main()async
  {
+   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+   await Future.delayed(const Duration(seconds: 1));
+   await Future.delayed(const Duration(seconds: 1));
+   await Future.delayed(const Duration(seconds: 1));
+   await Future.delayed(const Duration(seconds: 1));
+   await Future.delayed(const Duration(seconds: 1));
+   FlutterNativeSplash.remove();
    runApp(MyApp());
+
  }
+
 
 
 
